229518cf6a44aca869b2150a685ae051c63c9965
Matthew Martnick <martnickmatthew@gmail.com>
2019-03-30 17:09:49 -0400

    initial commit

---
+++ ByteShortInt/src/com/mattMartnick/Main.java
@@ -0,0 +1,25 @@
+package com.mattMartnick;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // int has a width of 32
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+        int myTotal = (myMinValue/2);
+
+        // different data types are incompatible with each other
+
+        // byte has a width of 8
+        byte myMaxByteValue = 127;
+
+        // short has a width of 16
+        short myMaxShortValue = 32767;
+
+        // long has a width of 64
+        long myMaxLongValue = 9_223_372_036_854_807L;
+
+        // minimum value limit = the negative MaxValue - (-1)
+    }
+}
---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Megaman!");
+
+        int myFirstNumber = 15 + 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = 1000 - (myFirstNumber + mySecondNumber + myThirdNumber);
+
+        System.out.println(myTotal);
+    }
+}
