1bfbd9c0a00fa62f4eadc0c3c811d9170aaaea5e
Martnick1 <matthewmartnick@gmail.com>
2019-05-07 13:18:55 -0400

    update

--- ByteShortInt/src/com/mattMartnick/Main.java
+++ ByteShortInt/src/com/mattMartnick/Main.java
@@ -8,22 +8,25 @@ public class Main {
         int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647;
         int myTotal = (myMinValue/2);
+        System.out.println("myMaxValue = " + myMaxValue + " and " + "myTotal = " + myTotal);
+
 
         // different data types are incompatible with each other
 
         // byte has a width of 8
         byte myMaxByteValue = 127;
-        byte myNewByteValue = (byte) (myMaxByteValue/2);
+        byte myNewByteValue = (byte) (myMaxByteValue / 2);
         System.out.println("myNewByteValue = " + myNewByteValue);
 
         // short has a width of 16
         short myMaxShortValue = 32767;
-        short myNewShortValue = (short) (myMaxShortValue/2);
+        short myNewShortValue = (short) (myMaxShortValue / 2);
         System.out.println("myNewShortValue = " + myNewShortValue);
 
 
         // long has a width of 64
         long myMaxLongValue = 9_223_372_036_854_807L;
+        System.out.println("myMaxLongValue = " + myMaxLongValue);
 
         // minimum value limit = the negative MaxValue - (-1)
     }
