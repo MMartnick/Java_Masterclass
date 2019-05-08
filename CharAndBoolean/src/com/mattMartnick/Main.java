3a2f13bd1afa765c098f0248aa04cfd301548392
Martnick1 <matthewmartnick@gmail.com>
2019-05-07 20:57:10 -0400

    adding CharAndBoolean lesson

---
+++ CharAndBoolean/src/com/mattMartnick/Main.java
@@ -0,0 +1,20 @@
+package com.mattMartnick;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Standard character
+        char myChar = 'D';
+
+        // Unicode format
+        char uniChar = '\u017D';
+
+        System.out.println("Standard char output is: " + myChar);
+        System.out.println("Unicode char output is: " + uniChar);
+
+        boolean myBoolean = true;
+        System.out.println( "is it " + myBoolean + "?");
+
+
+    }
+}
